2f4dd953125b1095d549d16ab6704bebeab7e7ae
Ryan-Bouanani <ryan42426@gmail.com>
2022-07-22 02:07:16 +0200

    Adding animations on scroll with Intersection Observer

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@
 const nav = document.querySelector('.navLinks');
 const openNavBtn = document.querySelector('nav #navToggleOpen');
 const closeNavBtn = document.querySelector('nav #navToggleClose');
+const elementsAppareance = document.querySelectorAll('.elementsScroll, h2')
 console.log(nav, openNavBtn, closeNavBtn);
 
 // open Navbar
@@ -20,4 +21,35 @@ const closeNav = () => {
     closeNavBtn.style.display = 'none';
 };
 closeNavBtn.addEventListener('click', closeNav);
+
+
+
+// * Appareance on scroll
+
+let options = {
+    // root: null
+    rootMargin: "-15% 0px",
+    threshold: 0
+}
+
+function handleIntersect(entries) {
+    console.log(entries);
+
+    entries.forEach(entry => {
+        // si entry est dans notre viewport alors opacity 1
+        if (entry.isIntersecting) {
+            entry.target.style.opacity = 1
+        }
+    });
+}
+
+const observer = new IntersectionObserver(handleIntersect, options);
+
+elementsAppareance.forEach(element => {
+    observer.observe(element);
+});
+
+
+
+
 
---
+++ public/mail.php
@@ -0,0 +1,30 @@
+<?php
+if (!empty($_POST['name']) && !empty($_POST['mail']) && !empty($_POST['object']) && !empty($_POST['message'])) {
+
+
+    // Expediteur
+    $to  = $_POST['name'];
+    
+    // Sujet
+    $subject = $_POST['object'];
+    
+    
+    // Message
+    $message = $_POST['name'] . ': '. $_POST['message'];
+    
+    // Pour envoyer un mail HTML, l'en-tête Content-type doit être défini
+    $headers  = 'MIME-Version: 1.0'."\r\n";
+    $headers .= 'Content-type: text/html; charset=iso-8859-1'."\n";
+    
+    // En-têtes additionnels
+    $headers  .= 'Reply-To: '. $_POST['mail'] . ".\n";
+    $headers  .= 'From: '. $to ."\n";
+    
+    // Envoi du mail
+    if (mail($to, $subject, $message, $headers)) {
+        echo 'le mail est envoye';
+    } else {
+        echo 'le mail n\'a pas été envoyer';
+    };
+}
+?>
